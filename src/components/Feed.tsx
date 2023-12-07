47da70399def9f7a21685b01b25c338f90b7761b
Burminskyi <burminskyi91@gmail.com>
2023-12-07 13:46:12 +0200

    Created navbar and main layout

---
+++ src/components/Feed.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export const Feed = () => {
+  return (
+    <Box
+    bgcolor="orange"
+    flex={4}
+    p={2}
+  >
+    Feed
+  </Box>
+  )
+}
